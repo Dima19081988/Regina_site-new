a031f87d16c08e483ca22ea8037839af9ac5f51c
Dima19081988 <kuznetsovdv2013@gmail.com>
2025-11-15 12:58:29 +0300

    new GHash in .gitignore

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,11 @@ import authRouter from './routes/auth.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use(session({
