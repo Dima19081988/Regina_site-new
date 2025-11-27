87dbdb839260716792771acd32529136333f843a
Dima19081988 <kuznetsovdv2013@gmail.com>
2025-11-27 13:46:15 +0300

    добавлена stats на AdminHomePage

--- frontend/src/pages/Homepage/AdminHomePage.tsx
+++ frontend/src/pages/Homepage/AdminHomePage.tsx
@@ -1,8 +1,46 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { pluralize } from "../../utils/pluralize";
 import styles from './Homepage.module.css';
 
 export default function AdminHomepage() {
     const navigate = useNavigate();
+
+    const [todayAppointments, setTodayAppointments] = useState<number>(0);
+    const [fileCount, setFilesCount] = useState<number>(0);
+    const [portfolioCount, setPortfolioCount] = useState<number>(0);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const loadData = async () => {
+            try {
+                const appointmentsRes = await fetch('http://localhost:3000/api/appointments/today', {
+                    credentials: 'include',
+                });
+                const appointments = await appointmentsRes.json();
+                setTodayAppointments(appointments.length);
+
+                const filesRes = await fetch('http://localhost:3000/api/files', {
+                    credentials:'include',
+                });
+                const files = await filesRes.json();
+                setFilesCount(files.length);
+
+                const portfolioRes = await fetch('http://localhost:3000/api/portfolio', {
+                    credentials: 'include',
+                })
+                const portfolio = await portfolioRes.json();
+                setPortfolioCount(portfolio.length);
+            } catch (err) {
+                console.error('Ошибка загрузки статистики:', err);
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadData()
+    }, []);
+
+    if (loading) return <div className={styles.hero}>Загрузка...</div>;
 
     const handleLogout = async () => {
         try {
@@ -39,6 +77,11 @@ export default function AdminHomepage() {
                     <li><Link to="/admin/appointments">Записи</Link></li>
                 </ul>
             </nav>
+            <div className={styles.stats}>
+                <div>Сегодня: {todayAppointments} {pluralize(todayAppointments)}</div>
+                <div>Файлов: {fileCount}</div>
+                <div>Записей в Портфолио: {portfolioCount}</div>
+            </div>
         </div>
     );
 }
